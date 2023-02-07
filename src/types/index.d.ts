deff2288a1a31f947c2ff00830d8ab2c441ae7af
Semmy <semmywong@126.com>
2023-02-07 18:01:29 +0800

    feat: add comment

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: Semmy Wong
+ * @Date: 2023-01-29 15:22:28
+ * @LastEditors: Semmy Wong
+ * @LastEditTime: 2023-02-07 17:47:33
+ * @Description: global type define
+ */
 interface OwlerOption {
     root: string;
     views: string;
